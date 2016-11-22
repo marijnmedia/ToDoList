f8f37cc3f622da03c545fa634ad8c95ef767b269
marijnmedia <mail@marijnmedia.nl>
2016-11-22 11:18:34 +0100

    Initial Commit

---
+++ MarijnHop-pset4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarijnHop-pset4
+//
+//  Created by Marijn Hop on 22/11/2016.
+//  Copyright © 2016 Marijn Hop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
